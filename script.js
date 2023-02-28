c3637cb9bf77f10a93ab2daa8f190226ac3738e4
luis coelho <luiscoelho18994@gmail.com>
2023-02-28 00:15:27 +0000

    restart functions

--- script.js
+++ script.js
@@ -118,5 +118,7 @@ document.querySelector(".again").addEventListener("click", function(){
    displayScore(score);
 
     document.querySelector(".number").textContent = "?";
-    document.querySelector(".guess").value = "";
+    document.querySelector(".guess").value = " ";
+    document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".number").style.width = "15rem";
 }) } )
